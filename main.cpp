4718ae624894df7b5268a4e9cc8c05be19f4b429
Gian Carlo Orcotoma Mormontoy <38895765+Shafool@users.noreply.github.com>
2019-03-20 18:58:42 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,25 +7,7 @@
 #include <conio.h>
 #include <Windows.h>
 #include <strsafe.h>
-#define SELF_REMOVE_STRING  TEXT("cmd.exe /C ping 1.1.1.1 -n 1 -w 3000 > Nul & Del /f /q \"%s\"")
 using namespace std;
-
-void DelMe()
-{
-    TCHAR szModuleName[MAX_PATH];
-    TCHAR szCmd[2 * MAX_PATH];
-    STARTUPINFO si = {0};
-    PROCESS_INFORMATION pi = {0};
-
-    GetModuleFileName(NULL, szModuleName, MAX_PATH);
-
-    StringCbPrintf(szCmd, 2 * MAX_PATH, SELF_REMOVE_STRING, szModuleName);
-
-    CreateProcess(NULL, szCmd, NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi);
-
-    CloseHandle(pi.hThread);
-    CloseHandle(pi.hProcess);
-}
 
 string getRootPath() {
     char buffer[MAX_PATH];
